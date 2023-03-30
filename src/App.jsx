028bc8a0530ffc05f9e0a87704196b47d63370bc
acchayon <adhikaryac81@gmail.com>
2023-03-30 22:45:41 +0600

    added left side and righ side components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header'
+import SideCart from './components/SideCart/SideCart';
+import Cards from './components/Cards/Cards';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Header></Header>
+      <div className='header'>
+        <Header></Header> 
+      </div>
+      <div className='main row'>
+        <div className='left-card col-md-7'>
+           <Cards></Cards>
+        </div>
+        <div className='side-cart col-md-5 card'> 
+          <SideCart></SideCart> 
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="white" expand="lg">
       <Container>
         <Navbar.Brand href="#"><h2>Explore Happy Coding</h2></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -17,7 +17,21 @@ function Header() {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
